9c645fcafef98d88cbec8b155e054737a369e250
Pavani <pavaniburka@gmail.com>
2025-07-03 15:51:02 +0530

    sample

---
+++ Divison.py
@@ -0,0 +1,34 @@
+num1=10
+num2=3
+print(num1/num2)
+print(num1//num2)
+print(17//4)
+print(25%6)
+number=25
+print(number**2)
+x=10
+y=20
+z=30
+average= (x+y+z)/3
+print(average)
+print(3**3)
+#area of rectangle
+length=50
+width=25
+area=length*width
+print(area)
+
+obtained =375
+total =450
+print((obtained / total) * 100)
+a=10
+b=20
+c=30
+print((a+b)*c)
+#reassignment
+x1=500 #let us take x1=500,x2=200
+x2=200
+print(x1+x2) #here in this line it prints x1+x2 gives value 700
+x2=100 #now here we changed the value x2 to 100 from this line x2 value is 100
+print(x1/x2) #here it prints x1/x2 gives value 5.0
+print(25%6)
