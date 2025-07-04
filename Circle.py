ff8439eb47c14043b93bb7436ac9f9b47ec3bddd
Pavani <166785821+pavani1131@users.noreply.github.com>
2025-07-04 21:56:24 +0530

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,7 @@
+import math
+radius=10
+perimeter=2*math.pi*radius
+area=math.pi*radius**2
+print("Radius",radius)
+print("Perimeter",perimeter)
+print("Area",area)
---
+++ temperature.py
@@ -0,0 +1,10 @@
+celsius=5
+fahrenheit=(celsius * 9/5) * 32
+print("Temperature in Celsius",celsius)
+print("Temperature in fahrenheit",fahrenheit)
+
+total_seconds=50
+hour=total_seconds//3600
+minute=(total_seconds//60) % 60
+second=total_seconds%60
+print(hour,"hours",minute,"minutes",second,"seconds")
